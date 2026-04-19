c3eb9e28052d4e1272a446940387d4f1b86c7803
2022636349 <128170694+2022636349@users.noreply.github.com>
2026-04-19 22:06:40 +0800

    精简版

---
+++ backend/src/main/java/com/campus/lostfound/modules/auth/dto/PasswordLoginReq.java
@@ -0,0 +1,16 @@
+package com.campus.lostfound.modules.auth.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+public class PasswordLoginReq {
+
+    @NotBlank
+    @Pattern(regexp = "^1[3-9]\\d{9}$", message = "手机号格式错误")
+    private String phone;
+
+    @NotBlank(message = "密码不能为空")
+    private String password;
+}
